743cee4f93c792a60192b81d49f8442de10f1805
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-02 09:58:57 +0700

    [UPDATE] Add get detail recipe

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ const app = express();
 const port = 3000;
 const database = require("./database");
 
+// Get All recipe
 app.get("/recipe", async (req, res) => {
   try {
     const request = await database`SELECT * FROM recipe`;
+
+    res.status(200).json({
+      status: true,
+      message: "Get data success",
+      data: request,
+    });
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+// Get detail recipe
+app.get("/recipe/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await database`SELECT * FROM recipe WHERE id = ${id}`;
 
     res.status(200).json({
       status: true,
