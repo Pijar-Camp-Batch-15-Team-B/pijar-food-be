7a2b176c95973c4cc30119940fc173fb3bdd07bc
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-11-04 02:37:27 +0700

    [UPD] add controllers comment

--- routers/comment.js
+++ routers/comment.js
@@ -1,53 +1,11 @@
 const router = require("express").Router();
-const database = require("../database");
+const commentController = require("../controllers/comment")
 
 //ENDPOINT COMMENT
 //Add comment
-router.post("/comment", async (req, res) => {
-    try {
-      const { recipe_id, username, photo_profile, message } = req.body;
-  
-      const request =
-        await database`INSERT INTO comment (recipe_id, username, photo_profile, message)
-        values
-        (${recipe_id}, ${username}, ${photo_profile}, ${message}) RETURNING id`;
-  
-      if (request.length > 0) {
-        res.status(201).json({
-          status: true,
-          message: "Insert Data Success",
-        });
-  
-        return;
-      }
-    } catch (error) {
-      res.status(502).json({
-        status: false,
-        message: "Something Wrong on our server",
-        data: [],
-      });
-    }
-  });
+router.post("/comment", commentController._addComment);
   
   //Get comment by recipe id
-  router.get("/recipe/comment/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const request =
-        await database`SELECT * FROM comment WHERE recipe_id = ${id}`;
-  
-      res.status(200).json({
-        status: true,
-        message: "Get data success",
-        data: request,
-      });
-    } catch (error) {
-      res.status(502).json({
-        status: false,
-        message: "Something wrong in our server",
-        data: [],
-      });
-    }
-  });
+  router.get("/recipe/comment/:id", commentController._getCommentByRecipe);
 
 module.exports = router;
